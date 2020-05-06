7c1fbfb87a080958e50a59767de337d9ff8be0f9
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-06 23:58:21 +0530

    Adding function to change the target valu

--- src/App.js
+++ src/App.js
@@ -20,12 +20,24 @@ const app=props => {
         ]
       });
     };
+    const nameCHnageHandler=(event)=>{
+      setPersonState({
+        persons:[
+          {name: "Max", age: 28},
+          {name: event.target.value, age: 29},
+          {name: "Stephanie", age: 27}
+        ]
+      });
+    }
     console.log(personState) 
     return (
       <div className="App">
         <h1> Hi this is a React App</h1>
         <Person name={personState.persons[0].name} age={personState.persons[0].age}>Hello There</Person>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age} click={()=> switchHandler("Maxi")}>Hello There</Person>
+        <Person name={personState.persons[1].name} age={personState.persons[1].age} click={()=> switchHandler("Maxi")}
+          changed={nameCHnageHandler}
+        >
+          Hello There</Person>
         <Person name={personState.persons[2].name} age={personState.persons[2].age}>Hello There</Person>
         <button onClick={()=> switchHandler("Maxmillian")}>Switch Names</button>
       </div>
