bf20ba4145fe9b99b623861457122130b4bbe48c
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-07 13:30:03 +0530

    Added conditional to show dynamic content

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ const app=props => {
       padding: '8px',
       cursor: 'pointer'
     };
+
     const [personState, setPersonState]=useState({
       persons:[
         {name: "Max", age: 28},
         {name: "Manu", age: 29},
         {name: "Stephanie", age: 26}
       ],
-      otherState: 'some other value'
+      otherState: 'some other value',
+      showPersons: false
     });
+
+    const toggleHandler=()=>{
+      const doesShow=personState.showPersons;
+      setPersonState({
+        persons:[
+          {name: "Max", age: 28},
+          {name: "Manu", age: 29},
+          {name: "Stephanie", age: 26}
+        ],
+        showPersons: !doesShow
+      })
+    }
     const switchHandler=(newName)=>{
       setPersonState({
         persons:[
@@ -38,14 +52,18 @@ const app=props => {
     console.log(personState) 
     return (
       <div className="App">
-        <h1> Hi this is a React App</h1>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}>Hello There</Person>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age} click={()=> switchHandler("Maxi")}
-          changed={nameChangeHandler}
-        >
-          Hello There</Person>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}>Hello There</Person>
-        <button style={style} onClick={()=> switchHandler("Maxmillian")}>Switch Names</button>
+        {
+          personState.showPersons?
+          <div>
+              <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+              <Person name={personState.persons[1].name} age={personState.persons[1].age} click={()=> switchHandler("Maxi")}
+              changed={nameChangeHandler}/>
+              <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+          </div>
+          : null
+        }
+        
+        <button style={style} onClick={toggleHandler}>Switch Names</button>
       </div>
     );
   
