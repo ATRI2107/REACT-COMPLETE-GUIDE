8095afd5500c8e8dcaae23a02e1ad0ada8ca7bba
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-22 22:58:56 +0530

    Added media query to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Radium from 'radium'
 import './Person.css'
 
  const person=(props)=>{
-     return(
-        <div className={"Person "+ props.classlist}>
+    const style={
+        '@media (min-width: 500px)':{
+            width: '450px'
+        }
+    }; 
+    return(
+        <div style={style} className={"Person "+ props.classlist}>
             <p onClick={props.click}> I am {props.name} and I'm {props.age} </p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed}></input>
         </div>
      ) 
  };
- export default person;
+ export default Radium(person);
